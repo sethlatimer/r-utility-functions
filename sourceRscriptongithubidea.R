8f51c45f139c6b970bd487b3e838a742e9324fdf
sethlatimer <sethlatimer@gmail.com>
2015-06-25 15:09:33 -0600

    added sourcer idea

---
+++ sourceRscriptongithubidea.R
@@ -0,0 +1,4 @@
+library(devtools)
+https://github.com/sethlatimer/r-utility-functions/blob/master/plotcars.R
+SourceURL <- "https://raw.githubusercontent.com/sethlatimer/r-utility-functions/master/plotcars.R"
+source_url(SourceURL)
